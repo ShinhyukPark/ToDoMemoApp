4f71525de884d05a48c48590925bffcbcc487a39
ShinhyukPark <parkgodhyuk@gmail.com>
2024-07-23 15:34:55 +0900

    Initial Commit

---
+++ ToDoMemoApp/ToDoMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMemoAppApp.swift
+//  ToDoMemoApp
+//
+//  Created by Shinhyuk Park on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
